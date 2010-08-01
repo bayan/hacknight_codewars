ef6968ba286459dc196a389c90bdc5bf65de2671
Bayan Khalili <bayan.khalili@siteminder.com.au>
2010-08-01 21:48:58 +1000

    Fixed typo and return all results in decoder

--- decoder.clj
+++ decoder.clj
@@ -4,28 +4,26 @@
 
 (use '[clojure.contrib.duck-streams :only (reader)])
 
-(def number-mapping [
-                     []
+(def number-mapping [[]
                      []
                      [\a \b \c]
                      [\d \e \f]
                      [\g \h \i]
-                     [\j \k \i]
+                     [\j \k \l]
                      [\m \n \o]
                      [\p \q \r \s]
                      [\t \u \v]
-                     [\w \x \y \z]
-                     ])
+                     [\w \x \y \z]])
 
 (def words (set (with-open [rdr (reader "/usr/share/dict/words")] (doall (line-seq rdr)))))
 
 (def encoded-text (re-seq #"\w+" "843 78425 27696 369 586733 6837 843 5299 364"))
 
 (defn decode [word]
-  (first (for [w (map #(apply str %)
-                      (apply cartesian-product
-                             (map (fn [digit] (number-mapping digit))
-                                  (for [char word] (Integer/parseInt (str char))))))
-               :when (words w)] w)))
+  (for [w (map #(apply str %)
+               (apply cartesian-product
+                      (map (fn [digit] (number-mapping digit))
+                           (for [char word] (Integer/parseInt (str char))))))
+        :when (words w)] w))
 
-(println (str-join " " (for [word encoded-text] (decode word))))
+(println (str-join " " (for [word encoded-text] (str-join "/" (decode word)))))
